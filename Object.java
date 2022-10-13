da20168dd2815308561f4be38cd5591505259245
mitchmeister44 <78915565+mitchmeister44@users.noreply.github.com>
2022-10-12 19:04:10 -0500

    Update Object.java

--- Object.java
+++ Object.java
@@ -20,3 +20,37 @@ public class BatterListing{
         return average;
     }
 }
+//Game class
+import java.util.Formatter;
+import java.util.Scanner;
+public class Game {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        
+        System.out.println("Please enter a player's name: ");
+        String userPlayerName = input.nextLine();
+        
+        System.out.println("Please enter your player's batting average: ");
+        double userPlayerAverage = input.nextDouble();
+        
+        String players[] = {userPlayerName};
+        double stats[] = {userPlayerAverage};
+        
+        Formatter outputFormat = new Formatter();
+        outputFormat.format("%15s      %15s\n","Player Name","Batting Average");
+        
+        for (String word : players) {
+            outputFormat.format("%14s",word);
+        }
+        for (double stat : stats) {
+            outputFormat.format("%14s",stat);
+        }
+        
+        
+        System.out.println(outputFormat);
+        
+        
+        input.close();
+    }
+}
